ca6320f104eb2afaaa7f8ee41c2103db53c80720
red <red@reds-MacBook-Pro.local>
2021-08-26 18:51:02 -0500

    Adj/usr/bin/python3 -m pip install -U autopep8ustments for Fahrenheit.

--- sensors/bme680.py
+++ sensors/bme680.py
@@ -1,8 +1,6 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-# Need to update for BME688
-
 import time
 import board
 import adafruit_bme680
@@ -12,7 +10,7 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
 # change this to match the location's pressure (hPa) at sea level
-bme680.sea_level_pressure = 1400 # For Chicago
+bme680.sea_level_pressure = 1013.25
 
 # You will usually have to add an offset to account for the temperature of
 # the sensor. This is usually around 5 degrees but varies by use. Use a
